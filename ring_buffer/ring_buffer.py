97a8ae301852464004efafcad8aaa1f5ad8254cf
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-11 12:12:40 -0600

    ring buffer mvp test passes

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,37 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        #Case 1 -- If the cache is not full then add to fill
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+       
+        # Case 2 -- If the cache is at its capacity remove head
+        # and replace it with a new value
+        elif self.current == self.storage.tail:
+            self.storage.remove_from_head()
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+        
+        #to retain appropriate order
+        else:
+            self.current.insert_after(item)
+            self.storage.length += 1
+            self.current = self.current.next
+            self.storage.delete(self.current.next)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        current = self.storage.head
+        #First we must loop through the cache 
+        while current is not None:
+            #append to fill the list
+            list_buffer_contents.append(current.value)
+            #repeat appending
+            current = current.next
 
         return list_buffer_contents
 
