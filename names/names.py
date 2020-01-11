26f66ff8d4970da98f443b31e5ab8f7646e28f9b
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-11 13:15:01 -0600

    incorporoated bst into names test/efficency improved to log n

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binarySearchTree import BinarySearchTree
 
 start_time = time.time()
 
@@ -10,11 +11,28 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+#takes ~ 5 seconds
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+#Using BST
+bst = BinarySearchTree('')
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+#First--- insert all the names from list 1 into a bst structure.
+for name in names_1:
+    bst.insert(name)
+
+for name in names_2:
+    # Next check our bst for duplicates
+    # compare strings by comparing their ascii value
+    if bst.contains(name): 
+        # if there is a match then its appended to duplcate list
+        duplicates.append(name)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
